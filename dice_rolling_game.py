c8df9671002c61ecc069a14b9777c44919cc3607
MrFxde <juan.space14@gmail.com>
2025-06-16 19:36:30 -0400

    optimized version

--- dice_rolling_game.py
+++ dice_rolling_game.py
@@ -9,18 +9,18 @@ MESSAGE = 'Roll the dice? (y/n): '
 
 while True:
     #print(MESSAGE)
-    input1 = input(MESSAGE)
-    input1 = input1.lower()
+    input1 = input(MESSAGE).lower()
 
     if (input1 != 'y') and (input1 != 'n'):
         print('Invalid Choice!')
     elif input1 == 'y':
         int1 = str(random.randint(1,6))
         int2 = str(random.randint(1,6))
-        print('(' + int1 + ', ' + int2 + ')')
+        print(f'({int1}), {int2}')
     elif input1 == 'n':
         print('Thanks for playing!')
         break
+    
     
     
    
